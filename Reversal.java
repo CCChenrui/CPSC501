d560352eda17b112de134facbf6c538236523f0f
Chenraywho@gmail.com <chenraywho@gmail.com>
2015-10-14 11:51:10 -0400

    Part of File input

--- Reversal.java
+++ Reversal.java
@@ -1,4 +1,36 @@
-// try some differences
-public class Reversal {
+import java.io.*;
+import java.util.ArrayList;
 
+
+public class Reversal {
+	public static void reverseFile(File input, File ouput) throws FileNotFoundException{
+		ArrayList<String> str=new ArrayList<String>();		
+		File file = new File ("input");
+		BufferedReader reader = null;
+		
+		
+		try{
+			reader = new BufferedReader (new FileReader(file));
+			String tempString = null;
+			
+			while ((tempString = reader.readLine()) != null ){
+				str.add(tempString);
+			}
+			
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			
+			if (reader != null ) {
+				try{
+					reader.close();
+				}catch (IOException e1){}
+			}
+			
+		}
+		
+		
+	}	
 }
