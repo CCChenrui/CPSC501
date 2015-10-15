4d99f3240befa4e8c621b2dc69c767321b758b60
chenruihu <chenraywho@gmail.com>
2015-10-14 21:01:26 -0400

    First Try

--- Reversal.java
+++ Reversal.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 
 
 public class Reversal {
-	public static void reverseFile(File input, File ouput) throws FileNotFoundException{
+	public static void reverseFile(File input, File output) throws FileNotFoundException{
 		ArrayList<String> str=new ArrayList<String>();		
-		File file = new File ("input");
 		BufferedReader reader = null;
 		
 		
 		try{
-			reader = new BufferedReader (new FileReader(file));
+			reader = new BufferedReader (new FileReader(input));
 			String tempString = null;
 			
 			while ((tempString = reader.readLine()) != null ){
@@ -21,8 +20,7 @@ public class Reversal {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally{
-			
+		} finally{		
 			if (reader != null ) {
 				try{
 					reader.close();
@@ -32,18 +30,55 @@ public class Reversal {
 		
 		for (int i=str.size()-1; i>=0 ; i--){
 			String line=(String)str.get(i);
+			char allline[]=line.toCharArray();
+			ArrayList<String> wordlist=new ArrayList<String>();
 			if (line != "/0") {
-				ArrayList<String> wordlist=new ArrayList<String>();
+				
 				String eachword = new String();
 				eachword="";
-				for (int j=line.length()-1; j>=0; j--){
-					if (line[j] !='a'){
-						
+				for (int j=allline.length-1; j>=0; j--){
+					if (allline[j] !=' '){
+						eachword += allline[j];
 					}
-					
+					else if (eachword!=""){
+						wordlist.add(eachword);
+						eachword="";
+					}				
 				}
 			}
-		}
-		
+			FileOutputStream fos = null;
+			BufferedWriter writer = null;
+			fos = new FileOutputStream(output);
+			writer = new BufferedWriter(new OutputStreamWriter(fos));
+			try{				
+				for (int j = wordlist.size()-2; j>=0 ; j--){
+					String wordstr=(String)wordlist.get(j);
+					char word[] = wordstr.toCharArray();
+					for (int l = word.length-1; l>=0; l--){
+						writer.write(word[l]);
+					}
+					writer.write(' ');
+				}
+				int j = wordlist.size()-1;
+				String wordstr=(String)wordlist.get(j);
+				char word[] = wordstr.toCharArray();
+				for (int l = word.length-1; l>=0; l--){
+					writer.write(word[l]);
+				}
+				writer.write("\r\n");
+				
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{		
+				if (reader != null ) {
+					try{
+						reader.close();
+					}catch (IOException e1){}
+				}			
+			
+			
+		}		
 	}	
+   }
 }
