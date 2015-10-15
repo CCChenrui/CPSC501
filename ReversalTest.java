05e9e30162f201986ce9b6ca936d9206d2d062a8
chenrui <chenrui@chenrui-laptop.(none)>
2015-10-15 19:36:54 -0400

    add some comments

--- ReversalTest.java
+++ ReversalTest.java
@@ -1,12 +1,18 @@
-import static org.junit.Assert.*;
+//Author : Chenrui Hu
+import java.io.*;
+import org.junit.*;
 
-import org.junit.Test;
-
+// there will be 4 different cases in ReversalTest
 public class ReversalTest {
 
+	@BeforeClass //Open the File
+	
+	@AfterClass //Close the File
+	
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		File actual =  
 	}
 
 }
