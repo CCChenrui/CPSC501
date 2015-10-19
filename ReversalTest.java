957e26c7d84d49aabb351700cf6894af82149c25
chenruihu <chenraywho@gmail.com>
2015-10-19 16:17:42 -0400

    delete something I don't need

--- ReversalTest.java
+++ ReversalTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import java.io.*;
 import java.util.Scanner;
 import org.junit.Test;
-<<<<<<< HEAD
 
 // there will be 4 different kinds of cases in ReversalTest
 // normal lines
@@ -47,59 +46,6 @@ public class ReversalTest {
 		
 			File Inputfile  = new File("TestCaseInput.txt");
 			File Outputfile = new File("TestCaseOutput.txt");
-=======
-
-// there will be 4 different cases in ReversalTest
-public class ReversalTest {
-
-	@Test
-	public void test() {
-		
-		try{
-			File Inputfile  = new File("TestCase1Input.txt");
-			File Outputfile = new File("TestCase1Output.txt");
-			
-			PrintWriter writer = new PrintWriter(Inputfile);
-			
-			writer.println("My name is CHenrui.");
-
-			writer.close();
-			
-			
-			Reversal.reverseFile(Inputfile, Outputfile);
-			
-			Scanner scan = new Scanner(Outputfile);
-
-		    String Expected= "CHenrui. is name My";
-			
-		    String Actual = scan.nextLine();
-		    
-		    scan.close();
-		    
-		    assertEquals("",Expected,Actual);
-			
-		} catch (IOException e){
-			fail("No exception should be thrown");
-		}
-
-
-	}
-	
-	@Test
-	public void test1() {
-		
-		try{
-			File Inputfile  = new File("TestCase1Input.txt");
-			File Outputfile = new File("TestCase1Output.txt");
-			
-			PrintWriter writer = new PrintWriter(Inputfile);
-			
-			writer.println("My name is CHenrui.");
-			writer.println("");
-			writer.println("This is my testcase.");
-			writer.close();
-			
->>>>>>> c28cc8df08f65f91268e141cc9a608c757b2e7df
 			
 			PrintWriter writer = new PrintWriter(Inputfile);		
 			writer.println("My name is Chenrui.");
@@ -107,7 +53,6 @@ public class ReversalTest {
 			writer.close();
 						
 			Reversal.reverseFile(Inputfile, Outputfile);
-<<<<<<< HEAD
 						
 			Scanner scan = new Scanner(Outputfile);
 
@@ -150,30 +95,6 @@ public class ReversalTest {
 		    
 		   
 		    scan.close();
-=======
-			
-			
-			
-			Scanner scan = new Scanner(Outputfile);
-
-		    String Expected= "testcase. my is This";
-			
-		    String Actual = scan.nextLine();
-		    		    
-		    assertEquals("",Expected,Actual);
-		    
-		    Expected= "CHenrui. is name My";
-			
-		    Actual = scan.nextLine();
-		    		    
-		    assertEquals("",Expected,Actual);
-		    
-		    scan.close();
-		} catch (IOException e){
-			fail("No exception should be thrown");
-		}
-
->>>>>>> c28cc8df08f65f91268e141cc9a608c757b2e7df
 
 	}
 
